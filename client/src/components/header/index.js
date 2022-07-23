ffd50596d3643f9308b8a4b6ce1889509593403e
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-23 14:23:43 -0400

    Styled the slogan, and fixed the styles that extened to other buttons by making it a separate class

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -7,16 +7,16 @@ export default function Header() {
     return (
         <nav>
             <div>
-                <button className='logo' onClick={() => alert("this is our home")}>[ &nbsp;]</button>
+                <button className='logo navbutton' onClick={() => alert("this is our home")}>[ &nbsp;]</button>
             </div>
             <div>
-                <button  onClick={() => alert("This is our website")}>Learn</button>
+                <button className='navbutton' onClick={() => alert("This is our website")}>Learn</button>
             </div>
             <div>
-                <button onClick={() => alert("Stay safe")}>Safety</button>
+                <button className='navbutton' onClick={() => alert("Stay safe")}>Safety</button>
             </div>
             <div>
-                <button onClick={() => alert("Need help? Me too!!")}>Support</button>
+                <button className='navbutton' onClick={() => alert("Need help? Me too!!")}>Support</button>
             </div>
         </nav>
     )
