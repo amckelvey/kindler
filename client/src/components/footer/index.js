48a7385fa9613d2016d5a887b5f4860b7d3671c9
shay <shayosullivan@hotmail.com>
2022-07-23 13:29:25 -0400

    footer githubs added

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -6,16 +6,15 @@ export default function Footer() {
             <span>
         © 2022 Kindler, Inc.
         </span>
-    <ul class='list-inline'>
-      <li class='list-inline-item'><a href='https://github.com/Lunirs' target='_blank'>Dan </a>
+    <ul className='list-inline'>
+      <li className='list-inline-item'><a href='https://github.com/Lunirs' target='_blank'>Dan </a>
       </li>
-      <li class='list-inline-item'><a href='https://github.com/lyf703331869' target='_blank'>Yafei Liu</a></li>
-      <li class='list-inline-item'><a href='https://github.com/amckelvey' target='_blank'></a>Andrew</li>
-      <li class='list-inline-item'><a href='https://github.com/shayosullivan' target='_blank'>Shay O'Sullivan</a></li>
-      <li class='list-inline-item'><a href='https://github.com/saukchung' target='_blank'></a>Brian</li>
-       <li class='list-inline-item'><a href='https://github.com/' target='_blank'></a>Tahsa</li>
+      <li className='list-inline-item'><a href='https://github.com/lyf703331869' target='_blank'>Yafei Liu</a></li>
+      <li className='list-inline-item'><a href='https://github.com/amckelvey' target='_blank'></a>Andrew</li>
+      <li className='list-inline-item'><a href='https://github.com/shayosullivan' target='_blank'>Shay O'Sullivan</a></li>
+      <li className='list-inline-item'><a href='https://github.com/saukchung' target='_blank'></a>Brian</li>
+      <li className='list-inline-item'><a href='https://github.com/' target='_blank'></a>Tahsa</li>
     </ul>
-  
         </ div>
     )
 }
