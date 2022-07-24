dd228af5213714ddf83e7778dfb73b64ac3fc14f
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-24 17:58:29 -0400

    added logout button to header component when the user is logged in

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './navMain.css'
+
+import LogoutBtn from "../LogoutBtn"
+
 import { Link } from "react-router-dom";
+import Auth from '../../utils/auth'
 
 export default function Header() {
-    
-    return (
-        <nav>
+    if (!Auth.loggedIn()) {
+        return (
+            <nav>
+                <div>
+                    <button className='logo navbutton' onClick={() => alert("this is our home")}>[ &nbsp;]</button>
+                </div>
+                <div>
+                    <button className='navbutton' onClick={() => alert("This is our website")}>Learn</button>
+                </div>
+                <div>
+                    <button className='navbutton' onClick={() => alert("Stay safe")}>Safety</button>
+                </div>
+                <div>
+                    <button className='navbutton' onClick={() => alert("Need help? Me too!!")}>Support</button>
+                </div>
+            </nav>
+        )
+    } else {
+        return (
+            <nav>
             <div>
                 <button className='logo navbutton' onClick={() => alert("this is our home")}>[ &nbsp;]</button>
             </div>
@@ -18,6 +39,10 @@ export default function Header() {
             <div>
                 <button className='navbutton' onClick={() => alert("Need help? Me too!!")}>Support</button>
             </div>
+            <div>
+                < LogoutBtn />
+            </div>
         </nav>
-    )
+        )
+    }
 }
