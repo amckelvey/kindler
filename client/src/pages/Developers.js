b897f0c2e2032111cf99151c2322bbf93c020b58
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-24 22:56:04 -0400

    Stuff

--- client/src/pages/Developers.js
+++ client/src/pages/Developers.js
@@ -1,8 +1,51 @@
-import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import Card from "../components/card";
+import Carousel from "../components/carousel";
 
 function App() {
+  let cards = [
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+      ),
+    },
+  ];
   return (
-    <h2>Carpe Daymn</h2>
+    <div className="">
+      <Carousel
+        cards={cards}
+        height="400px"
+        width="100%"
+        margin="150px auto"
+        offset={2}
+        showArrows={true}
+      />
+    </div>
   );
 }
 
