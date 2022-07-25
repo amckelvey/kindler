26d914d959a5a22ecb7c5f023f55ea03c2065277
Yafei Liu <yafeiliu@yafeis-air.myfiosgateway.com>
2022-07-25 09:43:35 -0400

    update

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import './navMain.css'
+import React from "react";
+import "./navMain.css";
 import { Link } from "react-router-dom";
 
 export default function Header() {
-    
-    return (
-        <nav>
-            <div>
-                <button className='logo navbutton' onClick={() => alert("this is our home")}>[ &nbsp;]</button>
-            </div>
-            <div>
-                <button className='navbutton' onClick={() => alert("This is our website")}>Learn</button>
-            </div>
-            <div>
-                <button className='navbutton' onClick={() => alert("Stay safe")}>Safety</button>
-            </div>
-            <div>
-                <button className='navbutton' onClick={() => alert("Need help? Me too!!")}>Support</button>
-            </div>
-        </nav>
-    )
+  const styles = {
+    btn: {
+      float: "right",
+    },
+  };
+
+  return (
+    <nav>
+      <div>
+        <button
+          className="logo navbutton"
+          onClick={() => alert("this is our home")}
+        >
+          [ &nbsp;]
+        </button>
+      </div>
+      <div>
+        <button
+          className="navbutton"
+          onClick={() => alert("This is our website")}
+        >
+          Learn
+        </button>
+      </div>
+      <div>
+        <button className="navbutton" onClick={() => alert("Stay safe")}>
+          Safety
+        </button>
+      </div>
+      <div>
+        <button
+          className="navbutton"
+          style={styles.btn}
+          onClick={() => alert("Need help? Me too!!")}
+        >
+          Support
+        </button>
+      </div>
+    </nav>
+  );
 }
