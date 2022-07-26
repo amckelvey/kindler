31e70ca2af5a8c052f550d8641644153d206ca5b
Lunirs <dhong0925@gmail.com>
2022-07-26 11:21:20 -0400

    changed file path

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import "./index.css";
 
-import Support from "./pages/Support";
+import Support from "./pages/support";
 import DevCarousel from "./pages/Developers";
 import LandingPage from "./pages/LandingPage";
 import DevRecSignup from "./pages/DevOrRecSignup";
@@ -74,7 +74,7 @@ function App() {
       <ApolloProvider client={client}>
         <Router>
           <div className="flex-column justify-flex-start min-100-vh">
-            <Header/>
+            <Header />
             <div style={styles.pageWrap}>
               <h1 style={styles.name}>
                 <span style={styles.LogoBracket}>[</span>KINDLER
