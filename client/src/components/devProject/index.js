b4ee089460cfb1a4c4d9a49d54b76579e70434ce
Yafei Liu <yafeiliu@yafeis-air.myfiosgateway.com>
2022-07-26 16:06:42 -0400

    pull

--- client/src/components/devProject/index.js
+++ client/src/components/devProject/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 function DevProject({ dev, num }) {
+  console.log(dev.projects[0].name);
   return (
     <div>
-      <h1>{num}</h1>
+      <h1>{dev.projects[0].name}</h1>
     </div>
+
+    // const projArr = Array.from(dev.projects);
+    // const mappedProj = projArr.map((project) => {
+    //   return (
+    //     <div>
+    //       <h1>{project.name}</h1>
+    //     </div>
+    //   );
+    // });
+    // return <div>{mappedProj}</div>;
   );
-  //   const projArr = Array.from(dev.projects);
-  //   const mappedProj = projArr.map((project) => {
-  //     return (
-  //       <div>
-  //         <h1>{project.name}</h1>
-  //       </div>
-  //     );
-  //   });
-  //   return <div>{mappedProj}</div>;
 }
 
 export default DevProject;
