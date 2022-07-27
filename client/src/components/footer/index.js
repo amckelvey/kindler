ed0d6962fc9567e24f936d116d3b4a978fbf3bdb
Yafei Liu <yafeiliu@yafeis-air.myfiosgateway.com>
2022-07-26 21:17:29 -0400

    change footer width to 90%

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
   const styles = {
     footerContainer: {
       position: "relative",
-      width: "98%",
+      width: "90%",
       height: "2.5rem",
       display: "inline-block",
       fontFamily: "Source Code Pro, monospace",
@@ -14,17 +14,13 @@ export default function Footer() {
       marginTop: "100px",
       marginBottom: "100px",
       flex: "1",
-      
-      paddingLeft: "3vw",
-      padding: "20px"
-      
 
+      paddingLeft: "3vw",
+      padding: "20px",
     },
 
     listInlineItem: {
-    display: "inline-block"
-
-
+      display: "inline-block",
     },
 
     h5: {
@@ -52,8 +48,6 @@ export default function Footer() {
 
     a: {
       color: "#CE9178",
-  
-    
     },
 
     whtSemiColon: {
@@ -72,64 +66,63 @@ export default function Footer() {
   };
 
   return (
-  
     <div className="footer" style={styles.footerContainer}>
       <div className="footer" style={styles.LeftBorder}>
         <h5 style={styles.h5}>
-              {" "}
-              <span style={styles.curlyBrackYellow}>&#123;</span>'KINDLER 2022'
-              <span style={styles.curlyBrackYellow}>&#125;</span>{" "}
-              <span style={styles.whtSemiColon}>;</span>{" "}
-            </h5>
-       <div className="footer" style={styles.LeftBorder}>
-        <h5 style={styles.h5}>
           {" "}
-          <span style={styles.curlyBrackYellow}>&#123;</span>'CREATOR'
+          <span style={styles.curlyBrackYellow}>&#123;</span>'KINDLER 2022'
           <span style={styles.curlyBrackYellow}>&#125;</span>{" "}
           <span style={styles.whtSemiColon}>;</span>{" "}
         </h5>
-        <ul className="list-inline" style={styles.ul}>
-        <span style={styles.curlyBrackYellow}>[</span>
-          <li className="list-inline-item" style={styles.listInlineItem}>
-            <a style={styles.a} href="https://github.com/Lunirs">
-              Dan
-            </a>
-            <span style={styles.whtSemiColon}>,</span>
-          </li>
-          <li className="list-inline-item" style={styles.listInlineItem}>
-            <a style={styles.a} href="https://github.com/lyf703331869">
-              Yafei
-            </a>
-            <span style={styles.whtSemiColon}>,</span>
-          </li>
-          <li className="list-inline-item" style={styles.listInlineItem}>
-            <a style={styles.a} href="https://github.com/amckelvey">
-              Andrew
-            </a>
-            <span style={styles.whtSemiColon}>,</span>
-          </li>
-          <li className="list-inline-item" style={styles.listInlineItem}>
-            <a style={styles.a} href="https://github.com/shayosullivan">
-              Shay
-            </a>
-            <span style={styles.whtSemiColon}>,</span>
-          </li>
-          <li className="list-inline-item" style={styles.listInlineItem}>
-            <a style={styles.a} href="https://github.com/saukchung">
-              Brian
-            </a>
-            <span style={styles.whtSemiColon}>,</span>
-          </li>
-          <li className="list-inline-item" style={styles.listInlineItem}>
-            <a style={styles.a} href="https://github.com/taa2153">
-              Tasha
-            </a>
-            <span style={styles.whtSemiColon}>,</span>
-          </li>
-          <span style={styles.curlyBrackYellow}>]</span>
-        </ul>
+        <div className="footer" style={styles.LeftBorder}>
+          <h5 style={styles.h5}>
+            {" "}
+            <span style={styles.curlyBrackYellow}>&#123;</span>'CREATOR'
+            <span style={styles.curlyBrackYellow}>&#125;</span>{" "}
+            <span style={styles.whtSemiColon}>;</span>{" "}
+          </h5>
+          <ul className="list-inline" style={styles.ul}>
+            <span style={styles.curlyBrackYellow}>[</span>
+            <li className="list-inline-item" style={styles.listInlineItem}>
+              <a style={styles.a} href="https://github.com/Lunirs">
+                Dan
+              </a>
+              <span style={styles.whtSemiColon}>,</span>
+            </li>
+            <li className="list-inline-item" style={styles.listInlineItem}>
+              <a style={styles.a} href="https://github.com/lyf703331869">
+                Yafei
+              </a>
+              <span style={styles.whtSemiColon}>,</span>
+            </li>
+            <li className="list-inline-item" style={styles.listInlineItem}>
+              <a style={styles.a} href="https://github.com/amckelvey">
+                Andrew
+              </a>
+              <span style={styles.whtSemiColon}>,</span>
+            </li>
+            <li className="list-inline-item" style={styles.listInlineItem}>
+              <a style={styles.a} href="https://github.com/shayosullivan">
+                Shay
+              </a>
+              <span style={styles.whtSemiColon}>,</span>
+            </li>
+            <li className="list-inline-item" style={styles.listInlineItem}>
+              <a style={styles.a} href="https://github.com/saukchung">
+                Brian
+              </a>
+              <span style={styles.whtSemiColon}>,</span>
+            </li>
+            <li className="list-inline-item" style={styles.listInlineItem}>
+              <a style={styles.a} href="https://github.com/taa2153">
+                Tasha
+              </a>
+              <span style={styles.whtSemiColon}>,</span>
+            </li>
+            <span style={styles.curlyBrackYellow}>]</span>
+          </ul>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
